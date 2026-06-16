97cd67d1b35d79a5544a2b5e0cd7987608015f0c
Draax17 <dgfglez@gmail.com>
2026-06-15 21:43:53 -0600

    import

--- src/preprocesamiento.py
+++ src/preprocesamiento.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+from IPython.display import display
 
 from sklearn.preprocessing import StandardScaler
 from sklearn.cluster import KMeans
